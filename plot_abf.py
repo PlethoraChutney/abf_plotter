68408300fd2e7e7c41b300ce8668b44879e8c2fd
Rich Posert <rich@posert.com>
2021-01-12 14:55:45 -0800

    Working annotations

--- plot_abf.py
+++ plot_abf.py
@@ -19,11 +19,31 @@ ax.plot(abf.sweepX, abf.sweepY, color = 'black')
 
 ax.set(xlabel = 'Time (sec)', ylabel = 'Clamp Current (pA)')
 
+# add horizontal tag lines
+yvals = []
+xmins = []
+xmaxes = []
+colors = []
+legends = []
 for i, tagTimeSec in enumerate(abf.tagTimesSec):
-    posX = abf.tagTimesSec[i]
+    start_x = abf.tagTimesSec[i]
+    try:
+        end_x = abf.tagTimesSec[i+1]
+    except IndexError:
+        end_x = max(abf.sweepX)
     comment = abf.tagComments[i]
-    color = 'C%d' % (i + 1)
-    ax.axvline(posX, label = comment, color = color, ls = '--')
+    color = 'C%d' % (int(comment[-3]))
+    yvals.append(10)
+    xmins.append(start_x)
+    xmaxes.append(end_x)
+    colors.append(color)
+    legends.append(comment[-3])
+
+ax.hlines(yvals, xmins, xmaxes, colors = colors, linewidths = 3)
+
+for y, xmin, xmax, color in zip(yvals, xmins, xmaxes, colors):
+    mid = (xmin + xmax) / 2
+    ax.text(mid, y + 7, color[-1], horizontalalignment = 'center')
 
 # this also hides the frame and axes
 pyabf.plot.scalebar(abf = abf)
